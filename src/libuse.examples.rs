01989b4567f14a2cdb3142a77de3f66a1c479afa
PabloGCH <pablochoconi@gmail.com>
2023-04-15 14:13:22 -0300

    Functions

--- src/libuse.examples.rs
+++ src/libuse.examples.rs
@@ -1,25 +1,3 @@
-//CHATGPT EXAMPLE
-/*
-use chatgpt::prelude::*;
-use chatgpt::types::*;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    // Getting the API key here
-    let key = std::env::args().nth(1).unwrap_or("".to_string());
-    println!("Key: {}", key);
-    let client = ChatGPT::new(key)?;
-    let response: CompletionResponse = client
-        .send_message("Name 5 blue things")
-        .await?;
-    println!("Response: ");
-    println!("{}", response.message().content);
-    Ok(())
-}
-*/
-
-
-
 
 //SYNTAX HIGHLIGHT TEST
 /*
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,42 @@
 use std::env::args;
 use chatgpt::prelude::*;
-use futures::stream::{self, StreamExt};
 use futures_util::StreamExt;
 use std::io::{stdout, Write};
 
-/// Requires the `streams` crate feature
+static mut BUFFER: String = String::new();
+
+fn update_buffer(new_string: String) {
+    unsafe {
+        BUFFER.push_str(new_string.as_str());
+        std::process::Command::new("clear").status().unwrap();
+        print!("{}", BUFFER);
+        // Manually flushing the standard output, as `print` macro does not do that
+        stdout().lock().flush().unwrap();
+    }
+}
+
+async fn get_response(query :String, client: ChatGPT) {
+    let stream = client
+        .send_message_streaming(query.as_str())
+        .await;
+    // Iterating over stream contents
+    stream.unwrap().for_each(|each| async move {
+        match each {
+            ResponseChunk::Content { delta, response_index: _,} => {
+                update_buffer(delta);
+                println!("")
+            }
+            _ => {}
+        }
+    })
+    .await;
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // Creating a client
     let key = args().nth(1).unwrap();
     let client = ChatGPT::new(key)?;
-    let stream = client
-        .send_message_streaming("Could you name me a few popular Rust backend server frameworks?")
-        .await?;
-
-    // Iterating over stream contents
-    stream
-        .for_each(|each| async move {
-            match each {
-                ResponseChunk::Content {
-                    delta,
-                    response_index: _,
-                } => {
-                    // Printing part of response without the newline
-                    print!("{delta}");
-                    // Manually flushing the standard output, as `print` macro does not do that
-                    stdout().lock().flush().unwrap();
-                }
-                _ => {}
-            }
-        })
-    .await;
-    ok(())
+    get_response("Give me a hello world program in 3 different languages".to_string(), client).await;
+    Ok(())
 }
